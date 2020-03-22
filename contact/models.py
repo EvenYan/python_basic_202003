ee0030e6acb620ded67aa84a3fba2bd3f2e8c930
evenyan <evenyan@evenyandeMacBook-Pro.local>
2020-03-22 15:20:16 +0800

    添加用户信息数据模型

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=40)
+    phone_num = models.CharField(max_length=11)
+
+    def __str__(self):
+        return self.name
